8c10457e732e1ffbe29b21984fc28204f1deb646
Evangellos Wiegers (eaw568) <evan.wiegers@usask.ca>
2020-04-17 22:46:47 -0600

    restructured project

---
+++ Main.py
@@ -0,0 +1,13 @@
+#ResumeBuilder
+#Evangellos Wiegers
+#April 17, 2020
+
+from .Support import Skill, SkillSection, Award, Education, Experience, Reference, Other, scrape, build
+
+
+
+
+
+
+if __name__ == '__main___':
+    build()
--- Support/__init__.py
+++ Support/__init__.py
@@ -1,14 +1,3 @@
-#ResumeBuilder
-#Evangellos Wiegers
-#April 17, 2020
-
 from .Qualifications import Skill, SkillSection, Award, Education, Experience, Reference, Other
 from .Scraper import scrape
 from .Builder import build
-
-
-
-
-
-if __name__ == '__main___':
-    build()
